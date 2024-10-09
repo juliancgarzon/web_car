f256df950dbb1ccf04c2429fd849e6d349b21ef8
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-10-09 01:34:48 -0500

    add styles and more changes

---
+++ js/mostrador.js
@@ -0,0 +1,9 @@
+const thumbnails = document.querySelectorAll('.thumbnail');
+const mainImage = document.querySelector('.main-image');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        mainImage.src = thumbnail.src;   
+
+    });
+});
