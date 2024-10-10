3576457f36fad7ebfe209238b3295c411f4b9b58
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-10-10 18:41:27 -0500

    create contacto .html and login.html

---
+++ js/login.js
@@ -0,0 +1,45 @@
+window.onload = (event) => {
+    const loginForm = document.getElementById ('loginForm');
+    const loginMessage = document.getElementById('loginMessage');
+
+    //const apiUrl = 'https://node-project-ql7a.vercel.app';
+
+    loginForm.addEventListener('submit', async function(event){
+
+        event.preventDefault(); // Prevenir que se envie el formulario de manera tradicional.
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        try {
+            //const response = await fetch(`${apiUrl}/api/login`, {
+            const response = await fetch(`http://localhost:3000/api/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password}),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            //const encodeData = btoa(data);
+            const encodeData = btoa(JSON.stringify(data));
+            console.log(encodeData);
+
+            if (response.ok) {
+                loginMessage.textContent = 'Login Exitoso';
+                loginMessage.style.color = 'green'
+                window.location.href = `catalogo.html#${encodeData}`;
+            } else {
+                loginMessage.textContent = data.message || 'Error en el Login';
+                loginMessage.style.color = 'red'
+            }
+        } catch (error) {
+            console.log (error)
+            loginMessage.textContent = 'Hubo un Error en el Login';
+            loginMessage.style.color = 'red'
+        }
+    
+    });
+};
